8cb405e9e0e26bc2d688bd4ebf09b7bfa7316919
Adbeel99 <103293313+Adbeel99@users.noreply.github.com>
2025-10-15 13:26:01 -0600

    semana 4 y 5
    avance

---
+++ Tienda_AdbeelEspinoza/src/test/java/com/tienda/Adbeel/domain/AdbeelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.Adbeel.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdbeelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
