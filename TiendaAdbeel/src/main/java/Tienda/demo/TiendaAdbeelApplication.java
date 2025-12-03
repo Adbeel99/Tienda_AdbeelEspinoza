95d99420e7df42a2b571a540987ab939a53b1e8d
Adbeel99 <103293313+Adbeel99@users.noreply.github.com>
2025-12-03 15:13:00 -0600

    arreglando errores

--- TiendaAdbeel/src/main/java/Tienda/demo/TiendaAdbeelApplication.java
+++ TiendaAdbeel/src/main/java/Tienda/demo/TiendaAdbeelApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TiendaSamuelApplication {
+public class TiendaAdbeelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaSamuelApplication.class, args);
+		SpringApplication.run(TiendaAdbeelApplication.class, args);
 	}
 
 }
